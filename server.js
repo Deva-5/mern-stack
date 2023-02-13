31c3744bbc1b15a65c836417b22cd9c58ccabe3e
Deva <devakumartadicherla2001@gmail.com>
2023-02-13 23:29:05 +0530

    add html page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
-	res.send("Iam Deva");
+	res.sendFile(__dirname+"/index.html");
 });
 
 app.listen(port, function(){
